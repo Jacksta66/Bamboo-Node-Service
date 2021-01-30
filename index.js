a31f9eca58bef36abfb8145c9216fd3d1c5aca38
Jack <jack.dillon266@gmail.com>
2021-01-31 01:06:49 +1100

    Get environment release info from build key and environment name.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 require('dotenv').config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
-
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
 
 const serverPort = process.env.SERVER_PORT;
@@ -13,58 +10,74 @@ const bambooUrl = process.env.BAMBOO_API_URL;
 const bambooToken = process.env.BAMBOO_TOKEN;
 
 const axiosInstance = axios.create({
-    headers: {'Authorization': 'Bearer ' + bambooToken}
-});
-
-// Get related deployment plans for build key
-// e.g http://{YourHost}/rest/api/latest/deploy/project/forPlan?planKey=SH-SHB
-app.get("/bamboo/deployment-plan/:plan_key", (req, res) => {
-    const planKey = req.params.plan_key;
-    axiosInstance.get(bambooUrl + "deploy/project/forPlan/", {
-        params: {
-            planKey: planKey
-        }
-    }).then(function(response) {
-        res.send(response.data);
-    }).catch(function(error) {
-        console.log(error);
-        res.send({})
-    });
-});
-
-// Get environments
-// e.g http://{YourHost}/rest/api/latest/deploy/project/1015809
-app.get("/bamboo/environments/:deployment_id", (req, res) => {
-    const deploymentId = req.params.deployment_id;
-    axiosInstance.get(bambooUrl + "deploy/project/" + deploymentId)
-        .then(function(response) {
-        res.send(response.data);
-    }).catch(function(error) {
-        console.log(error);
-        res.send({})
-    });
-});
-
-// Get deployments (this endpoint needs auth with a user that has read access to the environment)
-// e.g http://{YourHost}/rest/api/latest/deploy/environment/1081347/results?max-results=1&start-index=0
-app.get("/bamboo/environment/:environment_id", (req, res) => {
-    const environmentId = req.params.environment_id + "/";
-    const maxResults = req.query.max_results;
-    const startIndex = req.query.start_index;
-
-    const params = {
-        "max-results": maxResults,
-        "start-index": startIndex
+    headers: {
+        'Authorization': 'Bearer ' + bambooToken
     }
-
-    axiosInstance.get(bambooUrl + "deploy/environment/" + environmentId + "results", { params })
-        .then(function(response) {
-            res.send(response.data);
-        }).catch(function(error) {
-        console.log(error);
-        res.send({})
-    });
 });
+
+/**
+ * Gets c
+ *
+ * @param {string} plan_key The key for the bamboo build plan.
+ * @param {string} environment_name The name of the deployment environment.
+ * @param {number} max_results (Optional) The max number of results.
+ * @param {number} start_index (Optional) The index of which result to start at.
+ *
+ * @return {JSON} Release info for the specified environment.
+ */
+app.get("/bamboo/environment-info/:plan_key/:environment_name",
+    (req, res) => {
+        const planKey = req.params.plan_key;
+        const environmentName = req.params.environment_name;
+        const maxResults = req.query.max_results;
+        const startIndex = req.query.start_index;
+
+        // Get related deployment plans for build key
+        // e.g http://{YourHost}/rest/api/latest/deploy/project/forPlan?planKey=SH-SHB
+        console.log(`Getting deployment projects for plan key: ${planKey}`);
+        axiosInstance.get(bambooUrl + "deploy/project/forPlan/", {
+            params: {
+                planKey: planKey
+            }
+        }).then((response) => {
+            console.log(`Got following deployment projects: ${JSON.stringify(response.data)}`);
+            // Get environments
+            // e.g http://{YourHost}/rest/api/latest/deploy/project/1015809
+            axiosInstance.get(bambooUrl + "deploy/project/" + response.data[0].id)
+                .then((response) => {
+                    console.log(`Bamboo returned the following environments for plan key ${planKey}: ${JSON.stringify(response.data)}`);
+                    let environment = response.data.environments.filter(env => env.name === environmentName);
+                    if (Array.isArray(environment) && environment.length) {
+                        console.log(`environment found: ${JSON.stringify(environment)}`);
+                        let environmentId = environment[0].id;
+                        const params = {
+                            "max-results": maxResults,
+                            "start-index": startIndex
+                        }
+                        console.log(`Get Environment information for environment ID: ${environmentId}`);
+                        // Get deployments
+                        // e.g http://{YourHost}/rest/api/latest/deploy/environment/1081347/results?max-results=1&start-index=0
+                        axiosInstance.get(bambooUrl + "deploy/environment/" + environmentId + "/results", {
+                            params
+                        }).then((response) => {
+                            res.send(response.data);
+                        }).catch((error) => {
+                            console.log(error);
+                            res.sendStatus(500);
+                        });
+                    } else {
+                        console.log(`Error no deployment plan found for [${environmentName}]!`);
+                        res.sendStatus(404);
+                    }
+                }).catch((error) => {
+                console.log(error);
+                res.sendStatus(500);
+            });
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+    });
 
 app.listen(serverPort, () => {
     console.log("Bamboo service started on port: " + serverPort);
